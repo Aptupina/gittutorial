a82373d32968a782b5b26dcc60f88a9a842bf289
Aptupina <arturtsoy7@gmail.com>
2023-11-28 01:58:05 +0600

    please

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import logging
+import json
+
+app = Flask(__name__)
+
+logging.basicConfig(level = logging.DEBUG)
+
+@app.route("/", methods = ["POST"])
+def main():
+    logging.info(request.json)
+
+    response = {
+        "version": request.json["version"],
+        "session": request.json["session"],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    req = request.json
+    if req["session"]["new"]:
+        response["response"]["text"] = "Привет"
+        
+    return json.dumps(response)
