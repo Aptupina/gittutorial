ee407461c6c66d579564a6725eaaf004e4d13be4
Aptupina <arturtsoy7@gmail.com>
2023-11-28 02:58:28 +0600

    no

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def webhook():
-    try:
-        data = request.json
-        text = data.get('request', {}).get('command')
-        response_text = "Assalamu aleykum"
-
-        response = {
-            'response': {
-                'text': response_text,
-                'end_session': False
-            },
-            'version': '1.0'
-        }
-
-Send response to the provided webhook
-        webhook_url = "https://hkdk.events/656RgpXWrrjC"
-        requests.post(webhook_url, json=response)
-
-        return jsonify(response)
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-if name == 'main':
-    app.run(debug=True)
+@app.route('/alice', methods = ['POST'])
+def resp():
+    text = request.json.get('request', {}).get('command')
+    response_text = f'Вы сказали {text}'
+    response = {
+        'response': {
+            'text': response_text,
+            'end_session': False,
+            'suggests': ['йоу']
+        },
+        'version': '1.0'
+    }
+    return response
