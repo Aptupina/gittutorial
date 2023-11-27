8379939bf5eaa2af32a9ea81d11d79a5fa206443
Aptupina <arturtsoy7@gmail.com>
2023-11-28 02:55:51 +0600

    no

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 # Навык Алисы для Расписания Высшей Школы Экономики
 ## Алиса, запусти навык Расписание Вышки
 
 PRs приветствуются
 
 Хостинг – Yandex.Cloud.Functions. [Инструкция по развертыванию](https://cloud.yandex.ru/docs/functions/solutions/alice-skill).
+=======
+# gittutorial
+>>>>>>> parent of a82373d (please)
---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook():
+    try:
+        data = request.json
+        text = data.get('request', {}).get('command')
+        response_text = "Assalamu aleykum"
+
+        response = {
+            'response': {
+                'text': response_text,
+                'end_session': False
+            },
+            'version': '1.0'
+        }
+
+Send response to the provided webhook
+        webhook_url = "https://hkdk.events/656RgpXWrrjC"
+        requests.post(webhook_url, json=response)
+
+        return jsonify(response)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if name == 'main':
+    app.run(debug=True)
